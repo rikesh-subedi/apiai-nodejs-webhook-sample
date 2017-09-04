42b696132864ea3556211b480c252276d2d79da0
Subedi, Rikesh <rikesh.subedi@sap.com>
2017-09-04 19:57:35 +0530

    added intent switch

--- index.js
+++ index.js
@@ -14,6 +14,9 @@ app.get('/webhook', function (req, res) {
   res.send('You must POST your request')
 })
 
+var userSession = {}
+
+//https://hoo-dy.herokuapp.com/webhook
 app.post('/webhook', function (req, res) {
   // we expect to receive JSON data from api.ai here.
   // the payload is stored on req.body
@@ -35,17 +38,88 @@ app.post('/webhook', function (req, res) {
   console.log('* Received action -- %s', req.body.result.action)
 
   // parameters are stored in req.body.result.parameters
-  var userName = req.body.result.parameters['given-name']
-  var webhookReply = 'Hello ' + userName + '! Welcome from the webhook.'
-
-  // the most basic response
+  var result = req.body.result
+  var intentName = getIntentNameLowerCase(result)
+  var webhookReply = ""
+  var displayText = ""
+  var followupEvent = null
+  switch (intentName) {
+    case "menu":
+      console.log("received menu intent")
+      var mealType = paramaterFor(result, "meal")
+      var mealPreference = paramaterFor(result, "food-preference");
+      console.log("meal: "+ mealType)
+      console.log("preference: " + mealPreference)
+      webhookReply = getMenu(mealPreference)
+      displayText = webhookReply
+      followupEvent = {
+      "name": "take_order",
+      "data": {}
+         
+      }
+      break;
+    case "checkin":
+      webhookReply = "Hi,your checkin is successful. Checkin ID: " + (Math.floor(Math.random() * 100000));
+      displayText = displayText
+    case "welcome user":
+      var userName = paramaterFor(result, "given-name");
+      //webhookReply = 'Hello ' + userName + '! Welcome from the webhook.'
+      webhookReply = "Hi, your have got " + (Math.floor(Math.random() * 10) % 6 + 1);
+      displayText = webhookReply
+      // the most basic response
+      break
+    default: break;
+  }
+  
+  console.log(webhookReply)
   res.status(200).json({
     source: 'webhook',
     speech: webhookReply,
     displayText: webhookReply
   })
+
+
+
+
+
 })
 
 app.listen(app.get('port'), function () {
   console.log('* Webhook service is listening on port:' + app.get('port'))
 })
+
+
+function paramaterFor(result, parameterName) {
+  return result.parameters[parameterName]
+}
+function getIntentNameLowerCase(result) {
+  return result.metadata.intentName.toLowerCase()
+}
+
+function getMenu(mealPreference) {
+  console.log("inside getMenu:" + mealPreference);
+  switch(mealPreference) {
+    case "veg":
+        return getVegMeals().join(",")
+    case "non-veg":
+        return getNonVegMeals().join(",")
+    case "eggetarian":
+        return getEggMeals().join(",")
+    default:
+        return getVegMeals().concat(getNonVegMeals()).concat(getEggMeals()).join(",")
+  }
+
+}
+
+function getVegMeals(){
+  return ["paneer", "kofta"]
+}
+
+function getNonVegMeals() {
+  return ["chicken salad", "lamb ghost"]
+
+}
+
+function getEggMeals() {
+  return ["egg curry"]
+}
